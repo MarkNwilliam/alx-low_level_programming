52c2418ebf7a4059b6e1f9c609e88f9f40a4d6e6
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2022-10-18 16:56:05 -0700

    q

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,13 @@
-// Online C compiler to run C program online
-#include <stdio.h>
+#include <unistd.h>
 
-int main() {
-    // Write C code here
-    
-    printf("_putchar \n");
-
-    return 0;
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
